c9154260b0de33ff6c1158506742512bb2dabbc3
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-10 20:35:33 +0100

    Repeat ad infinitum

--- script.js
+++ script.js
@@ -2,36 +2,42 @@ let userWinsCount = 0
 let ties = 0
 let computerWinsCount = 0
 
-const userChoice = prompt('Please make your choice of R, P or S')
+while (true) {
+  const userChoice = prompt('Please make your choice of R, P or S')
 
-const computerChoice = getComputerChoice()
+  const computerChoice = getComputerChoice()
 
-alert(`You chose ${userChoice}, I chose ${computerChoice}`)
+  alert(`You chose ${userChoice}, I chose ${computerChoice}`)
 
-if (userChoice === 'R' && computerChoice === 'S') {
-  userWins()
-}
-if (userChoice === 'R' && computerChoice === 'P') {
-  computerWins()
-}
-if (userChoice === 'S' && computerChoice === 'R') {
-  computerWins()
-}
-if (userChoice === 'S' && computerChoice === 'P') {
-  userWins()
-}
-if (userChoice === 'P' && computerChoice === 'R') {
-  userWins()
-}
-if (userChoice === 'P' && computerChoice === 'S') {
-  computerWins()
-}
-if (userChoice === computerChoice) {
-  draw()
-}
+  if (userChoice === 'R' && computerChoice === 'S') {
+    userWins()
+  }
+  if (userChoice === 'R' && computerChoice === 'P') {
+    computerWins()
+  }
+  if (userChoice === 'S' && computerChoice === 'R') {
+    computerWins()
+  }
+  if (userChoice === 'S' && computerChoice === 'P') {
+    userWins()
+  }
+  if (userChoice === 'P' && computerChoice === 'R') {
+    userWins()
+  }
+  if (userChoice === 'P' && computerChoice === 'S') {
+    computerWins()
+  }
+  if (userChoice === computerChoice) {
+    draw()
+  }
 
-alert(`You have won ${userWinsCount} times, I have won ${computerWinsCount} times, and we've tied ${ties} times`)
+  alert(`You have won ${userWinsCount} times, I have won ${computerWinsCount} times, and we've tied ${ties} times`)
 
+  const playAgain = confirm('OK to play again, CANCEL to stop')
+  if (!playAgain) {
+    break
+  }
+}
 
 function userWins() {
   userWinsCount++
