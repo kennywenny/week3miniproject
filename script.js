c36aea8db4ffbae46db6db9b97b1ec249c491ec0
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-10 20:22:12 +0100

    Dummy function to get computer's choice

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ console.log('Hello world')
 
 const userChoice = prompt('Please make your choice of R, P or S')
 
-console.log(userChoice)
+const computerChoice = getComputerChoice()
+
+console.log(`You chose ${userChoice}`)
+console.log(`I chose ${computerChoice}`)
+
+function getComputerChoice() {
+  return 'R' //TODO: Make random
+}
