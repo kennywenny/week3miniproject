ab78985fceb25141a9d12f78dd8732342597e511
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-10 20:25:40 +0100

    If clauses to work out who wins

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const userChoice = prompt('Please make your choice of R, P or S')
 
 const computerChoice = getComputerChoice()
 
+if (userChoice === 'R' && computerChoice === 'S') {
+  // user wins
+}
+if (userChoice === 'R' && computerChoice === 'P') {
+  // computer wins
+}
+if (userChoice === 'S' && computeChoice === 'R') {
+  // computer wins
+}
+if (userChoice === 'S' && computerChoice === 'P') {
+  // user wins
+}
+if (userChoice === 'P' && computerChoice === 'R') {
+  // user wins
+}
+if (userChoice === 'P' && computerChoice === 'S') {
+  // computer wins
+}
+if (userChoice === computerChoice) {
+  // draw
+}
+
 console.log(`You chose ${userChoice}`)
 console.log(`I chose ${computerChoice}`)
 
